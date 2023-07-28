42a90f26bb901e27c63ce0de4b26a055d698ac10
CuongLe68 <cuongle.23012001@gmail.com>
2023-07-29 00:35:46 +0700

    update mongoose version 7.0.0 to 7.3.3

--- frontend/src/components/HomeUser/LandingPage/LandingPage.tsx
+++ frontend/src/components/HomeUser/LandingPage/LandingPage.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from "react-i18next";
 
 import logoIcon from "../../assets/images/logo-icon.png";
 import useHandlePageBar from "../../../_helper/useHandlePageBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function LandingPage() {
   const { t } = useTranslation();
@@ -10,12 +12,35 @@ function LandingPage() {
   //Cập nhật page cho navbar
   useHandlePageBar("sidebar_1");
 
-  return <div className='landingPage-container'>
-    <div className="left">
-      left 1
-    </div>
-    <div className="right">right</div>
-  </div>;
+  return (
+    <>
+      <div className='poppup'>Thông báo</div>
+      <div className='landingPage-container'>
+        <div className='wrap'>
+          <div className='head'>
+            <div className='left'>
+              <div className='search'>
+                <input type='text' placeholder="Nhập tên mẫu template"/>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </div>
+              <div className='category'>Danh mục</div>
+              <select className="list" name='' id=''>
+                <option value='1'>Mới nhất</option>
+                <option value='2'>Sử dụng nhiều</option>
+                <option value='3'>Đánh giá tốt nhất</option>
+              </select>
+            </div>
+            <div className="create-page">
+              <FontAwesomeIcon icon={faPlus} />
+              <p>Tạo trang mới</p>
+            </div>
+          </div>
+          <div className='body'>bo đỳ nè</div>
+          <div className='bottom'>chuyển trang</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
